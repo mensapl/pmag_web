0e201f1e932145c3c8599f136c93a06e8c994a51
Bartosz Bonisławski <bartosz.bonislawski@netguru.co>
2016-12-03 13:55:01 +0100

    add swagger

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   use_doorkeeper
   devise_for :users, skip: [:registrations]
+  mount GrapeSwaggerRails::Engine, at: "/documentation"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
