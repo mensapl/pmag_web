6d84927058a359329c5f141d8562311c7b5e1b46
Bartosz Bonisławski <bartosz.bonislawski@netguru.co>
2016-12-03 19:07:21 +0100

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 [
-  { mensa_id: 'PL111', email: 'qwe@qwe.qwe' },
-  { mensa_id: 'PL222', email: 'qwe2@qwe.qwe' },
-  { mensa_id: 'PL333', email: 'qwe3@qwe.qwe' },
-  { mensa_id: 'PL444', email: 'qwe4@qwe.qwe' }
+  { mensa_id: 'PL111', email: 'qwe@qwe.qwe', password: 'qweqweqwe' },
+  { mensa_id: 'PL222', email: 'qwe2@qwe.qwe', password: 'qweqweqwe' },
+  { mensa_id: 'PL333', email: 'qwe3@qwe.qwe', password: 'qweqweqwe' },
+  { mensa_id: 'PL444', email: 'qwe4@qwe.qwe', password: 'qweqweqwe' }
 ].each_with_index do |user_data, index|
-  User.find_or_create_by(user_data) do |user|
-    user.password = 'qweqweqwe'
+  user = User.find_by(mensa_id: user_data[:mensa_id])
+  unless user
+    user = User.create(user_data)
     token = Doorkeeper::AccessToken.create!(resource_owner_id: user.id, expires_in: nil)
     token.update(token: "test#{index}")
   end
