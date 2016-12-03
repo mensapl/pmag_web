92132148cb6253f29307b9f5fdad3153bbaabec2
Bartosz Bonisławski <bartosz.bonislawski@netguru.co>
2016-12-03 12:32:54 +0100

    add seeded users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
-user = User.find_or_create_by(email: 'qwe@qwe.qwe', 'qweqweqwe')
+[
+  { email: 'qwe@qwe.qwe' },
+  { email: 'qwe2@qwe.qwe' },
+  { email: 'qwe3@qwe.qwe' },
+  { email: 'qwe4@qwe.qwe' }
+].each do |user_data|
+  User.find_or_create_by(user_data) do |user|
+    user.password = 'qweqweqwe'
+  end
+end
